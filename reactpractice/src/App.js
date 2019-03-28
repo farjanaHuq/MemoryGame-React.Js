424124419b0b0c87110ea79ec33237be62005a86
Farjana Huq <fa.huq03@gmail.com>
2019-03-27 22:55:24 -0700

    reactpractice created

---
+++ reactpractice/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBrand from './components/navBrand'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Clicky-Game</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
